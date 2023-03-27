0d52c885ed2d5f62fff57d7e34ac9baf08eec3c7
jarkub <jared.kubly@gmail.com>
2023-03-27 16:30:35 -0600

    init

---
+++ src/AppClass.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Header from './components/Header'
+
+class App extends React.Component {
+    render() {
+        return <h1>Hello from a class</h1>
+    }
+}
+
+export default App
